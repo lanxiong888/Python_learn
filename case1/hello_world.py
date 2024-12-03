0fb9fcbb4d5a5362486885271f1a1391107615ca
alanaaaaa <773376729@qq.com>
2024-12-03 19:01:18 +0800

    hello world

---
+++ case1/hello_world.py
@@ -0,0 +1,19 @@
+#coding:utf8
+print("hello world")
+print("hello.py")
+print("hello_world.py")
+# Escape Characters
+# code notes
+'''
+print("\"hello\n\\wo\n\rrd\"")
+'''
+"""
+print("\"hello\n\\wo\n\rrd\"")
+"""
+print("-100abs",abs(-100))
+print("100.111abs",abs(100.111))
+'''
+print personal information
+'''
+print("name\t\tgender\t\thobby\t\taddress\nalan\t\tman\t\tplay games\t\tsichuan")
+
